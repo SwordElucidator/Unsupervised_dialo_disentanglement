43dbcffbf18f86d45a15198a47a61482d47b529d
Hao Sun <hao.sun1@veeva.com>
2021-12-17 14:18:31 +0800

    start to clean up the code

--- pretrain/message-pair-classifier/bert_train.py
+++ pretrain/message-pair-classifier/bert_train.py
@@ -1,23 +1,23 @@
-import os 
-import sys 
+import os
+import sys
 import argparse
-import json 
+import json
 import re
-from tqdm import tqdm 
-import numpy as np 
+from tqdm import tqdm
+import numpy as np
 from time import strftime, gmtime
 import logging
 
-import torch 
+import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from torch.nn import BCELoss
 from torch.utils.data import DataLoader, SequentialSampler
 from transformers import AdamW, get_linear_schedule_with_warmup
 
-import transformers 
+import transformers
 from transformers import BertForNextSentencePrediction, BertTokenizer, BertModel
-import apex 
+import apex
 
 
 class DatasetForBert(torch.utils.data.Dataset):
@@ -34,7 +34,7 @@ class DatasetForBert(torch.utils.data.Dataset):
         self.pad_id = pad_id
         self.mask_id = mask_id
         self.num_training_instances = num_training_instances
-    
+
     def __len__(self):
         return int(self.num_training_instances)
 
@@ -43,7 +43,7 @@ class DatasetForBert(torch.utils.data.Dataset):
             ids = ids[:max_len - 1]
         ids = ids + [self.sep_id]
         return ids
-    
+
     def __pad(self, ids, max_len):
         if len(ids) < max_len:
             return ids + [self.pad_id] * (max_len - len(ids))
@@ -56,18 +56,18 @@ class DatasetForBert(torch.utils.data.Dataset):
         feature = self.features[idx]
 
         source_id_1 = self.__trunk([self.cls_id] + feature["sent1"], self.max_source_len)
-        segment_id_1 = [0]*len(source_id_1) + [0]*(self.max_source_len-len(source_id_1))
-        input_mask_1 = [1]*len(source_id_1) + [0]*(self.max_source_len-len(source_id_1))
+        segment_id_1 = [0] * len(source_id_1) + [0] * (self.max_source_len - len(source_id_1))
+        input_mask_1 = [1] * len(source_id_1) + [0] * (self.max_source_len - len(source_id_1))
         input_id_1 = self.__pad(source_id_1, self.max_source_len)
 
         source_id_2 = self.__trunk([self.cls_id] + feature["sent2"], self.max_source_len)
-        segment_id_2 = [0]*len(source_id_2) + [0]*(self.max_source_len-len(source_id_2))
-        input_mask_2 = [1]*len(source_id_2) + [0]*(self.max_source_len-len(source_id_2))
+        segment_id_2 = [0] * len(source_id_2) + [0] * (self.max_source_len - len(source_id_2))
+        input_mask_2 = [1] * len(source_id_2) + [0] * (self.max_source_len - len(source_id_2))
         input_id_2 = self.__pad(source_id_2, self.max_source_len)
 
         label_id = feature["label"]
         return input_id_1, input_mask_1, segment_id_1, \
-                    input_id_2, input_mask_2, segment_id_2, label_id
+               input_id_2, input_mask_2, segment_id_2, label_id
 
 
 def read_data(filename):
@@ -86,11 +86,11 @@ def tokenize_data(train_data, tokenizer, filename):
         data = []
         for item in tqdm(train_data):
             sent1, sent2, label = item
-            sent1_id = tokenizer.convert_tokens_to_ids(sent1.split()) 
+            sent1_id = tokenizer.convert_tokens_to_ids(sent1.split())
             sent2_id = tokenizer.convert_tokens_to_ids(sent2.split())
             data.append({
-                'sent1': sent1_id, 
-                'sent2': sent2_id, 
+                'sent1': sent1_id,
+                'sent2': sent2_id,
                 'label': label,
             })
         torch.save(data, filename)
@@ -143,21 +143,23 @@ def batch_list_to_batch_tensors(batch):
 
 def train(args, model, tokenizer, training_data, logger):
     if args.fp16:
-        from apex import amp 
+        from apex import amp
     else:
         amp = None
-    
+
     checkpoint_state_dict = None
 
+    # set the device
     model.to(args.device)
+
     model, optimizer = prepare_for_training(args, model, checkpoint_state_dict, amp=amp)
-    
+
     # define the total batch size
     if args.n_gpu == 0 or args.no_cuda:
         per_node_train_batch_size = args.per_gpu_train_batch_size * args.gradient_accumulation_steps
     else:
         per_node_train_batch_size = args.per_gpu_train_batch_size * args.n_gpu * args.gradient_accumulation_steps
-        
+
     args.train_batch_size = per_node_train_batch_size
     global_step = 0
 
@@ -175,11 +177,11 @@ def train(args, model, tokenizer, training_data, logger):
     # dataset
     train_dataset = DatasetForBert(
         features=training_data,
-        max_source_len=args.max_source_len, 
-        max_seq_len=args.max_len, 
-        cls_id=tokenizer.cls_token_id, sep_id=tokenizer.sep_token_id, 
-        pad_id=tokenizer.pad_token_id, mask_id=tokenizer.mask_token_id, 
-        offset=args.train_batch_size * global_step, 
+        max_source_len=args.max_source_len,
+        max_seq_len=args.max_len,
+        cls_id=tokenizer.cls_token_id, sep_id=tokenizer.sep_token_id,
+        pad_id=tokenizer.pad_token_id, mask_id=tokenizer.mask_token_id,
+        offset=args.train_batch_size * global_step,
         num_training_instances=args.train_batch_size * args.num_training_steps,
     )
 
@@ -192,7 +194,7 @@ def train(args, model, tokenizer, training_data, logger):
     logger.info("  Total train batch size (w. parallel, distributed & accumulation) = %d", args.train_batch_size)
     logger.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
     logger.info("  Total optimization steps = %d", args.num_training_steps)
-    
+
     # The training features are shuffled
     train_sampler = SequentialSampler(train_dataset)
     train_dataloader = DataLoader(
@@ -217,7 +219,7 @@ def train(args, model, tokenizer, training_data, logger):
                     }
         if args.model_config.startswith('bert'):
             inputs_1['token_type_ids'] = batch[2]
-            
+
         sent1_vec = model(**inputs_1)[1]
 
         inputs_2 = {'input_ids': batch[3],
@@ -225,7 +227,7 @@ def train(args, model, tokenizer, training_data, logger):
                     }
         if args.model_config.startswith('bert'):
             inputs_2['token_type_ids'] = batch[5]
-            
+
         sent2_vec = model(**inputs_2)[1]
 
         score = torch.matmul(sent1_vec.unsqueeze(1), sent2_vec.unsqueeze(1).permute(0, 2, 1)).squeeze(-1).squeeze(-1)
@@ -241,7 +243,7 @@ def train(args, model, tokenizer, training_data, logger):
 
         if args.gradient_accumulation_steps > 1:
             loss = loss / args.gradient_accumulation_steps
-        
+
         if args.fp16:
             with amp.scale_loss(loss, optimizer) as scaled_loss:
                 scaled_loss.backward()
@@ -269,14 +271,16 @@ def train(args, model, tokenizer, training_data, logger):
                 os.makedirs(save_path, exist_ok=True)
                 model_to_save = model.module if hasattr(model, "module") else model
                 model_to_save.save_pretrained(save_path)
-                logger.info("Saving model checkpoint %d into %s", global_step, save_path) 
+                logger.info("Saving model checkpoint %d into %s", global_step, save_path)
 
 
 def prepare(args):
+    # start prepare
     args.current_time = strftime("%Y-%b-%d-%H_%M_%S", gmtime())
     args.output_dir = os.path.join(args.output_dir, args.data_name, args.aug_type, args.current_time)
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
+    # make dirs
     args.cache_dir = os.path.join(args.output_dir, args.cache_dir)
     if not os.path.exists(args.cache_dir):
         os.makedirs(args.cache_dir)
@@ -284,23 +288,25 @@ def prepare(args):
     args.cached_input_dir = os.path.join(args.cached_input_dir, args.data_name, args.aug_type)
     if not os.path.exists(args.cached_input_dir):
         os.makedirs(args.cached_input_dir)
-    
+
     # define the logger
     logger_name = os.path.join(args.output_dir, "log.txt")
     LOG_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO, filename=logger_name, filemode='a')
     logger = logging.getLogger()
 
+    # use cuda / cpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     args.n_gpu = torch.cuda.device_count()
-    
+
     logger_msg = '\n========================='
     logger.info(logger_msg)
     logger.info(json.dumps(args.__dict__, sort_keys=True, indent=4))
     args.device = device
 
-    if args.fp16:
+    if args.fp16:  # use Half-precision floating-point format
         try:
+            # use NVIDIA APEX
             apex.amp.register_half_function(torch, 'einsum')
             apex.amp.register_float_function(torch, 'sigmoid')
         except ImportError:
@@ -312,7 +318,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='../data/')
     parser.add_argument('--data_name', type=str, choices=['movie'], default='movie')
-    parser.add_argument('--aug_type', type=str, choices=['same_speaker', 'filtered_same_speaker', 'mix', 'gold'], default='same_speaker')
+    parser.add_argument('--aug_type', type=str, choices=['same_speaker', 'filtered_same_speaker', 'mix', 'gold'],
+                        default='same_speaker')
     parser.add_argument('--model_config', type=str, default='bert-base-uncased')
     parser.add_argument('--output_dir', type=str, default='output')
     parser.add_argument('--max_source_len', type=int, default=64)
@@ -336,25 +343,28 @@ def get_args():
     parser.add_argument('--fp16_opt_level', type=str, default='O1',
                         help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
                              "See details at https://nvidia.github.io/apex/amp.html")
-    
+
     args = parser.parse_args()
     return args
 
 
 def main():
     args = get_args()
+    # prepare dir / cuda / logger / ...
     logger = prepare(args)
 
+    # load pretrained bert model and tokenizer
     model, tokenizer = get_model_and_tokenizer(args.model_config)
-    
+
+    # get train json file and read the json data
     filename = os.path.join(args.data_dir, args.data_name, args.aug_type, 'train.json')
     train_data = read_data(filename)
 
+    # make tokenized data
     cached_tokenized_filename = os.path.join(args.cached_input_dir, "tokenized_train.pt")
     tokenized_data = tokenize_data(train_data, tokenizer, cached_tokenized_filename)
 
-    model.to(args.device)
-
+    # start bert training
     train(args, model, tokenizer, tokenized_data, logger)
 
 
